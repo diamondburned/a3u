989d06496c4cad7c6353a53367f3a532a1ed01ee
diamondburned (Forefront) <datutbrus@gmail.com>
2020-06-27 00:13:07 -0700

    Broke CLI API

--- main.go
+++ main.go
@@ -18,45 +18,60 @@ func init() {
 	log.SetPrefix("")
 }
 
+var slashesc = strings.NewReplacer("/", "∕", `\`, "⧵").Replace
+
 var (
 	trim   string
 	prefix string
 	output string = "-" // stdout default
+	extens string
 	simple bool
 )
 
 func init() {
 	os.Args[0] = filepath.Base(os.Args[0]) // cleaner
+
+	pflag.Usage = func() {
+		log.Println("Usage:", os.Args[0], "[flags...] path")
+		log.Println("  Path can be a directory, in which the application will make a file")
+		log.Println("  with the playlist name as the filename and sanitize it by substituting")
+		log.Println("  slashes.")
+		log.Println("")
+
+		log.Println("Flags:")
+		pflag.PrintDefaults()
+	}
 }
 
 func main() {
 	pflag.StringVarP(&trim, "trim", "t", trim, "Prefix to trim from URI")
 	pflag.StringVarP(&prefix, "prefix", "p", prefix, "Prefix to prepend to URI")
 	pflag.StringVarP(&output, "output", "o", output, "Path to write to, - for stdout")
+	pflag.StringVarP(&extens, "extension", "e", extens, "Extension to override in filename")
 	pflag.BoolVarP(&simple, "simple", "s", simple, "Output simple M3U format")
 	pflag.Parse()
 
-	var p m3u.Playlist
-
-	if len(pflag.Args()) == 0 {
-		p = append(p, convert(os.Stdin)...)
-	} else {
-		// If there are arguments, treat them as files.
-		for _, file := range pflag.Args() {
-			f, err := os.Open(file)
-			if err != nil {
-				log.Fatalln("Failed to open file:", err)
-			}
-
-			p = append(p, convert(f)...)
-			f.Close()
-		}
+	if len(pflag.Args()) != 1 {
+		pflag.Usage()
+		os.Exit(2)
 	}
 
-	var err error
+	f, err := os.Open(pflag.Arg(0))
+	if err != nil {
+		log.Fatalln("Failed to open file:", err)
+	}
+	f.Close()
+
+	name, tracks := convert(f)
+	p := m3u.Playlist(tracks)
 
 	var w = os.Stdout
-	if output != "-" {
+	if output != "-" && output != "" {
+		// Is the output is a directory, then use the playlist name instead.
+		if isdir(output) {
+			output = filepath.Join(output, slashesc(name))
+		}
+
 		w, err = os.OpenFile(output, os.O_WRONLY|os.O_TRUNC|os.O_CREATE, 0664)
 		if err != nil {
 			log.Fatalln("Failed to open file to write to:", err)
@@ -74,7 +89,7 @@ func main() {
 	}
 }
 
-func convert(r io.Reader) []m3u.Track {
+func convert(r io.Reader) (string, []m3u.Track) {
 	p, err := audpl.Parse(r)
 	if err != nil {
 		log.Fatalln("Failed to parse:", err)
@@ -88,12 +103,34 @@ func convert(r io.Reader) []m3u.Track {
 			continue
 		}
 
+		var path = prefix + strings.TrimPrefix(track.URI, trim)
+		if extens != "" {
+			path = convertExt(path, extens)
+		}
+
 		tracks = append(tracks, m3u.Track{
-			Path:  prefix + strings.TrimPrefix(track.URI, trim),
+			Path:  path,
 			Title: track.Title,
 			Time:  l,
 		})
 	}
 
-	return tracks
+	return p.Name, tracks
+}
+
+func convertExt(file string, ext string) string {
+	oldExt := filepath.Ext(file)
+	return file[:len(file)-len(oldExt)] + "." + ext
+}
+
+func isdir(path string) bool {
+	if len(path) > 0 && path[len(path)-1] == os.PathSeparator {
+		return true
+	}
+
+	if s, err := os.Stat(path); err == nil && s.IsDir() {
+		return true
+	}
+
+	return false
 }
